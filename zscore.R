5335b50e66d74bd0ba706b2c8ee31d13e20fccaf
luke-od <68912173+luke-od@users.noreply.github.com>
2022-04-11 22:53:44 +0100

    Update zscore.R

--- zscore.R
+++ zscore.R
@@ -1,3 +1,8 @@
+# x = a specific value u want to test
+# mu = population mean
+# s = standard deviation
+# n = sample size
+# tail = 1 or 2 tailed test
 
 z.test <- function(x, mu, s, n, tail){
   z <- (x-mu)/(s/sqrt(n))
@@ -9,10 +14,12 @@ z.test <- function(x, mu, s, n, tail){
 z.test(6, 5, 2, 20, 2)
 
 
-z.tested <-function(z, tail){
+p.value <-function(z, tail){
   if(tail==1) return(cat("z-score =", z, "\np-value =", pnorm(-abs(z))))
   if(tail==2) return(cat("z-score =", z, "\np-value =", 2*pnorm(-abs(z))))
   if(tail !=1 | tail !=2) return("can only be 1 or 2 tailed")
 }
 
-z.tested(1.96, 1)
+p.value(1.96, 1)
+# feel free to stop it from printing out the z-score u just put in
+#\n just puts it on a new line
